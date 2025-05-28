8a406ea2a5b056a0e5030d78fb168770faa03e24
kharissa28 <kharissasr911@gmail.com>
2025-05-28 07:47:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("welcome tu my apk")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "bebas"
 )
